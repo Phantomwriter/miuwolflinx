3bcd2ece7576ef91be84790d09631e7918c238ee
Phantomwritergit config --global user.nametakeovertheworld.livingston7@gmail.comgit config --global user.name takeovertheworld.livingston7@gmail.com <takeovertheworld.livingston7git config --global user.name takeovertheworld.livingston7@gmail.com>
2012-09-10 16:12:51 -0400

    newest version of miuwolflinx

---
+++ newMemberJson.js
@@ -0,0 +1,286 @@
+var json = {
+	"User 1" :{
+		"group":["Group:","Fun"],
+		"fname":["First name:","Howard Livingston"],
+		"pword":["Password:","Alibubba123$"],
+		"cpword":["Confirm Password:","Alibubba123$"],
+		"email":["Email:","takeovertheworld.livingston7@gmail.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","Tablet"],
+		"friends":["I have:","2"],
+		"date":["Date:","June 2,2012"],
+		"comments":["comments:","I wanna rock!"]
+	},	
+	"User 2" :{
+		"group":["Group:","Work'"],
+		"fname":["First name:","Tori Livingston"],
+		"pword":["Password:","PanicAtTheDisco"],
+		"cpword":["Confirm Password:","PanicAtTheDisco"],
+		"email":["Email:","tnight47@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","5"],
+		"date":["Date:","November 10, 2011"],
+		"comments":["comments:","I'm a pesky teenager"]
+	},
+	"User 3" :{
+		"group":["Group:","Education"],
+		"fname":["First name:","Hannah Ilagan"],
+		"pword":["Password:","PanicAtTheDisco"],
+		"cpword":["Confirm Password:","PanicAtTheDisco"],
+		"email":["Email:","kittensAndZombies@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","yay me!"]
+	},
+	
+	"User 4" :{
+		"group":["Group:","Education"],
+		"fname":["First name:","Alley Livingston"],
+		"pword":["Password:","Dungeons and Dragons"],
+		"cpword":["Confirm Password:","PanicAtTheDisco"],
+		"email":["Email:","kittensAndZombies@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","These aren't the droids you're looking for!"]
+	},
+	
+	"User 5" :{
+		"group":["Group:","I Do It All"],
+		"fname":["First name:","Don the Dude"],
+		"pword":["Password:","need food"],
+		"cpword":["Confirm Password:","need food"],
+		"email":["Email:","willplayforfood@aol.com"],
+		"selectValue":["What do you want to do:","Play Opponent"],
+		"deviceValue":["What device are you using:","tablet"],
+		"friends":["I have:","0"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Will play for food!"]
+	},
+
+	"User 6" :{
+		"group":["Group:","Education"],
+		"fname":["First name:","Murph"],
+		"pword":["Password:","twinkies"],
+		"cpword":["Confirm Password:","twinkies"],
+		"email":["Email:","gimmefood@aol.com"],
+		"selectValue":["What do you want to do:","PlayOpponent"],
+		"deviceValue":["What device are you using:","tablet"],
+		"friends":["I have:","1"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","I'm my own best friend!"]
+	},
+
+
+	"User 7" :{
+		"group":["Group:","I Do It All"],
+		"fname":["First name:","Bobby Burner"],
+		"pword":["Password:","smokey"],
+		"cpword":["Confirm Password:","smokey"],
+		"email":["Email:","MaryJane@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","yay me!"]
+	},
+
+
+	"User 8" :{
+		"group":["Group:","Work"],
+		"fname":["First name:","Heather"],
+		"pword":["Password:","RitzCarlton"],
+		"cpword":["Confirm Password:","RitzCarlton"],
+		"email":["Email:","MoreThanYou@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","10,000"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Send me a text!"]
+	},
+
+
+
+	"User 9" :{
+		"group":["Group:","Fun"],
+		"fname":["First name:","Mr. Jones"],
+		"pword":["Password:","Headhunter"],
+		"cpword":["Confirm Password:","Headhunter"],
+		"email":["Email:","wannajob@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","pc"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Call me for a job!"]
+	},
+
+
+
+	"User 10" :{
+		"group":["Group:","Work"],
+		"fname":["First name:","Killer"],
+		"pword":["Password:","Crossbones"],
+		"cpword":["Confirm Password:","Crossbones"],
+		"email":["Email:","dontbe scared@aol.com"],
+		"selectValue":["What do you want to do:","Computer Challenge"],
+		"deviceValue":["What device are you using:","pc"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","The tides of war are breaking the dam!!"]
+	},
+
+
+
+
+
+	"User 11" :{
+		"group":["Group:","Fun"],
+		"fname":["First name:","Granny Gerta"],
+		"pword":["Password:","Grandma"],
+		"cpword":["Confirm Password:","Grandma"],
+		"email":["Email:","loveInEveryStitch@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","pc"],
+		"friends":["I have:","1"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","You'll love my cookies, but hate my punishment!"]
+	},
+
+
+
+	"User 12" :{
+		"group":["Group:","I Do It All"],
+		"fname":["First name:","Bob the mechanic"],
+		"pword":["Password:","bent pipe"],
+		"cpword":["Confirm Password:","Tourque"],
+		"email":["Email:","CarKiller@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","tablet"],
+		"friends":["I have:","25"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Your car will never be the same"]
+	},
+
+
+
+
+	"User 13" :{
+		"group":["Group:","Fun"],
+		"fname":["First name:","Kelly"],
+		"pword":["Password:","And Like"],
+		"cpword":["Confirm Password:","And Like"],
+		"email":["Email:","Don'tEvenGoThere@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","tablet"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Don't you wanna' come party with me?"]
+	},
+
+
+	"User 14" :{
+		"group":["Group:","Work"],
+		"fname":["First name:","The Tin Woodsman"],
+		"pword":["Password:","oil can"],
+		"cpword":["Confirm Password:","oil can"],
+		"email":["Email:","INeedAHeart@aol.com"],
+		"selectValue":["What do you want to do:","Multi-Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","if I only had a heart"]
+	},
+
+	"User 15" :{
+		"group":["Group:","Education"],
+		"fname":["First name:","Scarecrow"],
+		"pword":["Password:","NoFire"],
+		"cpword":["Confirm Password:","NoFire"],
+		"email":["Email:","needABrain@aol.com"],
+		"selectValue":["What do you want to do:","Multi-Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","They tore off my arms and threw them over there!!"]
+	},
+
+
+
+
+	"User 16" :{
+		"group":["Group:","I Do It All"],
+		"fname":["First name:","The Cowardly Lion"],
+		"pword":["Password:","I ain't scared a' nutin'"],
+		"cpword":["Confirm Password:","I ain't scared a' nutin'"],
+		"email":["Email:","putEmUp@aol.com"],
+		"selectValue":["What do you want to do:","Multi-Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Whad' ya' have tp go and do that for?"]
+	},
+
+
+
+	"User 17" :{
+		"group":["Group:","Fun"],
+		"fname":["First name:","Dorothy"],
+		"pword":["Password:","Toto"],
+		"cpword":["Confirm Password:","Toto"],
+		"email":["Email:","Kansas@aol.com"],
+		"selectValue":["What do you want to do:","Multi-Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","There's no place like home!"]
+	},
+
+
+
+
+	"User 18" :{
+		"group":["Group:","I Do It All"],
+		"fname":["First name:","The wizard"],
+		"pword":["Password:","Come back tomorrow"],
+		"cpword":["Confirm Password:","Come back tomorrow"],
+		"email":["Email:","NeverMindThatManBehindTheCurtain@aol.com"],
+		"selectValue":["What do you want to do:","ComputerChallenge"],
+		"deviceValue":["What device are you using:","pc"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Nobody gets in to see the wizard, not no way, not no how!!"]
+	},
+
+
+	"User 19" :{
+		"group":["Group:","Fun"],
+		"fname":["First name:","Glenda the good witch"],
+		"pword":["Password:","Be gone"],
+		"cpword":["Confirm Password:","Be gone"],
+		"email":["Email:","GoodWitches@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","tablet"],
+		"friends":["I have:","10,000,000"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","Follow the yellow brick road"]
+	},
+
+	"User 20" :{
+		"group":["Group:","Work"],
+		"fname":["First name:","Wicked Witch of West"],
+		"pword":["Password:","My Pretty"],
+		"cpword":["Confirm Password:","My Pretty"],
+		"email":["Email:","WickedWitches@aol.com"],
+		"selectValue":["What do you want to do:","Text"],
+		"deviceValue":["What device are you using:","phone"],
+		"friends":["I have:","3"],
+		"date":["Date:","June 16, 2012"],
+		"comments":["comments:","I'm melting!"]
+	}
+}
+
+		
